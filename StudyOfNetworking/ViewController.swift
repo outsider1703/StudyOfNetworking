a74eb90a9d42dcdb26bf13f729c9faedfc0300b4
outsider1703 <simka-simka020697simka-simka1>
2020-09-08 22:23:56 +0200

    Initial Commit

---
+++ StudyOfNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudyOfNetworking
+//
+//  Created by Macbook on 08.09.2020.
+//  Copyright © 2020 Igor Simonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
